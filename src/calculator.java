043e9a4c16b2c1fc9783eae9a2795fd9e9ba5b8e
zolotoi005 <46765307+zolotoi005@users.noreply.github.com>
2019-01-19 16:40:02 -0500

    add division method

--- src/calculator.java
+++ src/calculator.java
@@ -12,5 +12,10 @@ public class calculator {
         return num1 * num2;
     
     }
+    
+    public static double divide (double num1, double num2){
+    return num1 / num2;
+    
+    }
         
 }
